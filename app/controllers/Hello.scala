85be0f83e2d2ead79277556feffa4ad54ceab474
Andrew Leverentz <andrew.leverentz@empiricotx.com>
2021-01-22 16:17:13 -0800

    Render simple Spark query (This works! Next, we need to look for conflicts between zetio / databricks-connect / playframework)

--- app/controllers/Hello.scala
+++ app/controllers/Hello.scala
@@ -3,9 +3,18 @@ package controllers
 import javax.inject.{Inject, Singleton}
 import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
 
+import org.apache.spark.sql.SparkSession
+
 @Singleton
 class Hello @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
   def mainPage: Action[AnyContent] = Action {
-    Ok("Hello, Play world!")
-  }  
+    val spark = SparkSession
+      .builder()
+      .master("local[*]")
+      .getOrCreate()
+    val results = spark.read.load("dbfs:/mnt/data/zetio/run/20201101.1/lookup/variant.type.lookup.parquet")
+      .limit(100)
+      .collect()
+    Ok(results.mkString("\n"))
+  }
 }
